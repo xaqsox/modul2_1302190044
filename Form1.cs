a78ff2c7149ed1fdb234aa3a628d00cbe8067486
Mochamad Aqsa <101115008+xaqsox@users.noreply.github.com>
2023-03-06 08:18:01 +0700

    nambahin fitur
    nambahin fitur kal

--- Form1.cs
+++ Form1.cs
@@ -7,50 +7,125 @@ namespace modul2_1302190044
             InitializeComponent();
         }
 
-        
+        void isiangka()
+        {
+            string angka1 = "1";
+            string angka2 = "2";
+            string angka3 = "3";
+            string angka4 = "4";
+            string angka5 = "5";
+            string angka6 = "6";
+            string angka7 = "7";
+            string angka8 = "8";
+            string angka9 = "9";
+            string angka0 = "0";
+        }
         private void btnangka1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "1";
-        }
+            if (int.Parse(textBox1.Text) != 0)
+            {
+                textBox1.Text += "1";
+            }
+            else
+            {
+                textBox1.Text = "1";
+            }
 
+        }
         private void btnangka2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "2";
+            if (int.Parse(textBox1.Text) != 0)
+            {
+                textBox1.Text += "2";
+            }
+            else
+            {
+                textBox1.Text = "2";
+            }
         }
 
         private void btnangka3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "3";
+            if (int.Parse(textBox1.Text) != 0)
+            {
+                textBox1.Text += "3";
+            }
+            else
+            {
+                textBox1.Text = "3";
+            }
         }
 
         private void btnangka4_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "4";
+            if (int.Parse(textBox1.Text) != 0)
+            {
+                textBox1.Text += "4";
+            }
+            else
+            {
+                textBox1.Text = "4";
+            }
         }
 
         private void btnangka5_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "5";
+            if (int.Parse(textBox1.Text) != 0)
+            {
+                textBox1.Text += "5";
+            }
+            else
+            {
+                textBox1.Text = "5";
+            }
         }
 
         private void btnangka6_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "6";
+            if (int.Parse(textBox1.Text) != 0)
+            {
+                textBox1.Text += "6";
+            }
+            else
+            {
+                textBox1.Text = "6";
+            }
         }
 
         private void btnangka7_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "7";
+            if (int.Parse(textBox1.Text) != 0)
+            {
+                textBox1.Text += "7";
+            }
+            else
+            {
+                textBox1.Text = "7";
+            }
         }
 
         private void btnangka8_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "8";
+            if (int.Parse(textBox1.Text) != 0)
+            {
+                textBox1.Text += "8";
+            }
+            else
+            {
+                textBox1.Text = "8";
+            }
         }
 
         private void btnangka0_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "0";
+            if (int.Parse(textBox1.Text) != 0)
+            {
+                textBox1.Text += "9";
+            }
+            else
+            {
+                textBox1.Text = "9";
+            }
         }
     }
 }
