a9ca2d477eb856546118cc058d5dfba060042921
Mochamad Aqsa <101115008+xaqsox@users.noreply.github.com>
2023-03-06 07:27:31 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace modul2_1302190044
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnangka1 = new System.Windows.Forms.Button();
+            this.btnangka2 = new System.Windows.Forms.Button();
+            this.btnangka3 = new System.Windows.Forms.Button();
+            this.btnangka4 = new System.Windows.Forms.Button();
+            this.btnangka8 = new System.Windows.Forms.Button();
+            this.btnangka7 = new System.Windows.Forms.Button();
+            this.btnangka6 = new System.Windows.Forms.Button();
+            this.btnangka5 = new System.Windows.Forms.Button();
+            this.btnangka0 = new System.Windows.Forms.Button();
+            this.btnangka9 = new System.Windows.Forms.Button();
+            this.btntmbh = new System.Windows.Forms.Button();
+            this.btn_samadengan = new System.Windows.Forms.Button();
+            this.btn_kurang = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(12, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 58);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnangka1
+            // 
+            this.btnangka1.Location = new System.Drawing.Point(12, 96);
+            this.btnangka1.Name = "btnangka1";
+            this.btnangka1.Size = new System.Drawing.Size(53, 35);
+            this.btnangka1.TabIndex = 1;
+            this.btnangka1.Text = "1";
+            this.btnangka1.UseVisualStyleBackColor = true;
+            this.btnangka1.Click += new System.EventHandler(this.btnangka1_Click);
+            // 
+            // btnangka2
+            // 
+            this.btnangka2.Location = new System.Drawing.Point(71, 96);
+            this.btnangka2.Name = "btnangka2";
+            this.btnangka2.Size = new System.Drawing.Size(53, 35);
+            this.btnangka2.TabIndex = 2;
+            this.btnangka2.Text = "2";
+            this.btnangka2.UseVisualStyleBackColor = true;
+            this.btnangka2.Click += new System.EventHandler(this.btnangka2_Click);
+            // 
+            // btnangka3
+            // 
+            this.btnangka3.Location = new System.Drawing.Point(130, 96);
+            this.btnangka3.Name = "btnangka3";
+            this.btnangka3.Size = new System.Drawing.Size(53, 35);
+            this.btnangka3.TabIndex = 3;
+            this.btnangka3.Text = "3";
+            this.btnangka3.UseVisualStyleBackColor = true;
+            this.btnangka3.Click += new System.EventHandler(this.btnangka3_Click);
+            // 
+            // btnangka4
+            // 
+            this.btnangka4.Location = new System.Drawing.Point(189, 96);
+            this.btnangka4.Name = "btnangka4";
+            this.btnangka4.Size = new System.Drawing.Size(53, 35);
+            this.btnangka4.TabIndex = 4;
+            this.btnangka4.Text = "4";
+            this.btnangka4.UseVisualStyleBackColor = true;
+            this.btnangka4.Click += new System.EventHandler(this.btnangka4_Click);
+            // 
+            // btnangka8
+            // 
+            this.btnangka8.Location = new System.Drawing.Point(189, 137);
+            this.btnangka8.Name = "btnangka8";
+            this.btnangka8.Size = new System.Drawing.Size(53, 35);
+            this.btnangka8.TabIndex = 8;
+            this.btnangka8.Text = "8";
+            this.btnangka8.UseVisualStyleBackColor = true;
+            this.btnangka8.Click += new System.EventHandler(this.btnangka8_Click);
+            // 
+            // btnangka7
+            // 
+            this.btnangka7.Location = new System.Drawing.Point(130, 137);
+            this.btnangka7.Name = "btnangka7";
+            this.btnangka7.Size = new System.Drawing.Size(53, 35);
+            this.btnangka7.TabIndex = 7;
+            this.btnangka7.Text = "7";
+            this.btnangka7.UseVisualStyleBackColor = true;
+            this.btnangka7.Click += new System.EventHandler(this.btnangka7_Click);
+            // 
+            // btnangka6
+            // 
+            this.btnangka6.Location = new System.Drawing.Point(71, 137);
+            this.btnangka6.Name = "btnangka6";
+            this.btnangka6.Size = new System.Drawing.Size(53, 35);
+            this.btnangka6.TabIndex = 6;
+            this.btnangka6.Text = "6";
+            this.btnangka6.UseVisualStyleBackColor = true;
+            this.btnangka6.Click += new System.EventHandler(this.btnangka6_Click);
+            // 
+            // btnangka5
+            // 
+            this.btnangka5.Location = new System.Drawing.Point(12, 137);
+            this.btnangka5.Name = "btnangka5";
+            this.btnangka5.Size = new System.Drawing.Size(53, 35);
+            this.btnangka5.TabIndex = 5;
+            this.btnangka5.Text = "5";
+            this.btnangka5.UseVisualStyleBackColor = true;
+            this.btnangka5.Click += new System.EventHandler(this.btnangka5_Click);
+            // 
+            // btnangka0
+            // 
+            this.btnangka0.Location = new System.Drawing.Point(130, 178);
+            this.btnangka0.Name = "btnangka0";
+            this.btnangka0.Size = new System.Drawing.Size(53, 35);
+            this.btnangka0.TabIndex = 10;
+            this.btnangka0.Text = "0";
+            this.btnangka0.UseVisualStyleBackColor = true;
+            this.btnangka0.Click += new System.EventHandler(this.btnangka0_Click);
+            // 
+            // btnangka9
+            // 
+            this.btnangka9.Location = new System.Drawing.Point(71, 178);
+            this.btnangka9.Name = "btnangka9";
+            this.btnangka9.Size = new System.Drawing.Size(53, 35);
+            this.btnangka9.TabIndex = 9;
+            this.btnangka9.Text = "9";
+            this.btnangka9.UseVisualStyleBackColor = true;
+       
+            // 
+            // btntmbh
+            // 
+            this.btntmbh.Location = new System.Drawing.Point(83, 238);
+            this.btntmbh.Name = "btntmbh";
+            this.btntmbh.Size = new System.Drawing.Size(53, 35);
+            this.btntmbh.TabIndex = 11;
+            this.btntmbh.Text = "+";
+            this.btntmbh.UseVisualStyleBackColor = true;
+            // 
+            // btn_samadengan
+            // 
+            this.btn_samadengan.Location = new System.Drawing.Point(179, 238);
+            this.btn_samadengan.Name = "btn_samadengan";
+            this.btn_samadengan.Size = new System.Drawing.Size(53, 35);
+            this.btn_samadengan.TabIndex = 12;
+            this.btn_samadengan.Text = "=";
+            this.btn_samadengan.UseVisualStyleBackColor = true;
+            // 
+            // btn_kurang
+            // 
+            this.btn_kurang.Location = new System.Drawing.Point(28, 238);
+            this.btn_kurang.Name = "btn_kurang";
+            this.btn_kurang.Size = new System.Drawing.Size(53, 35);
+            this.btn_kurang.TabIndex = 13;
+            this.btn_kurang.Text = "-";
+            this.btn_kurang.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 287);
+            this.Controls.Add(this.btn_kurang);
+            this.Controls.Add(this.btn_samadengan);
+            this.Controls.Add(this.btntmbh);
+            this.Controls.Add(this.btnangka0);
+            this.Controls.Add(this.btnangka9);
+            this.Controls.Add(this.btnangka8);
+            this.Controls.Add(this.btnangka7);
+            this.Controls.Add(this.btnangka6);
+            this.Controls.Add(this.btnangka5);
+            this.Controls.Add(this.btnangka4);
+            this.Controls.Add(this.btnangka3);
+            this.Controls.Add(this.btnangka2);
+            this.Controls.Add(this.btnangka1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kalkulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button btnangka1;
+        private Button btnangka2;
+        private Button btnangka3;
+        private Button btnangka4;
+        private Button btnangka8;
+        private Button btnangka7;
+        private Button btnangka6;
+        private Button btnangka5;
+        private Button btnangka0;
+        private Button btnangka9;
+        private Button btntmbh;
+        private Button btn_samadengan;
+        private Button btn_kurang;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+namespace modul2_1302190044
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void btnangka1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "1";
+        }
+
+        private void btnangka2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "2";
+        }
+
+        private void btnangka3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "3";
+        }
+
+        private void btnangka4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "4";
+        }
+
+        private void btnangka5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "5";
+        }
+
+        private void btnangka6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "6";
+        }
+
+        private void btnangka7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "7";
+        }
+
+        private void btnangka8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "8";
+        }
+
+        private void btnangka0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+    }
+}
